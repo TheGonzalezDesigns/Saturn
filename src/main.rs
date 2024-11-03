1a986199703e2671ef3c2da0b67789700dfa703d
TheGonzalezDesigns <thegonzalezdesigns@gmail.com>
2024-11-02 20:55:14 -0700

    feat: enable search module and update test results

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use core_modules::jarvis::jarvis::jarvis;
+use core_modules::search::search::search;
 use tokio::main;
 
 #[main]
 async fn main() {
-    let query: String = String::from("Who's winning the presidential race rn");
-    let response = jarvis(query).await.expect("Failed to call jarvis");
-    println!("Hello, world!: {response}");
+    let query: String = String::from("Who's in the last presidential race");
+    let search_query: String = String::from("Who's winning the presidential race rn");
+    let response = jarvis(query.clone()).await.expect("Failed to call jarvis");
+    println!("{query}: {response}");
+    let search_response = search(search_query.clone()).await.expect("Failed to call search");
+    println!("{search_query}: {search_response}");
 }
