7894dc265388ce76c69b391e21ba644ba2d8041b
TheGonzalezDesigns <thegonzalezdesigns@gmail.com>
2024-11-02 20:20:00 -0700

    feat: Introduce async main, use tokio, and make async call to Jarvis

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use core_modules::jarvis::jarvis::jarvis;
+use tokio::main;
+
+#[main]
+async fn main() {
+    let query: String = String::from("Who's winning the presidential race rn");
+    let response = jarvis(query).await.expect("Failed to call jarvis");
+    println!("Hello, world!: {response}");
 }
