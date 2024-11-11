67baa8f6efaae9cf07ac862ec69388aca4f5bfb7
TheGonzalezDesigns <thegonzalezdesigns@gmail.com>
2024-11-10 18:17:01 -0800

    feat: implement saturn

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
+use anyhow::Result;
+use core_modules::chat_completions::bots::saturn::saturn::saturn;
 use serde_json::json;
 use warp::{http::StatusCode, reply, serve, Filter, Rejection, Reply};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Server initiating...");
@@ -13,7 +16,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 async fn handle_query(query: serde_json::Value) -> Result<impl Reply, Rejection> {
-    let json_response = json!({ "online": true, "query": query });
-    let reply = reply::with_status(json_response.to_string(), StatusCode::OK);
+    let query: String = format!("{query}");
+    let reply = match saturn(query.clone()).await {
+        Ok(saturn_response) => {
+            let json_response = json!({ "query": query, "response": saturn_response });
+            reply::with_status(json_response.to_string(), StatusCode::OK)
+        }
+        Err(e) => {
+            let error = format!("{e}");
+            let json_response = json!({ "query": query, "error": error });
+            reply::with_status(json_response.to_string(), StatusCode::RESET_CONTENT)
+        }
+    };
     return Ok(reply);
 }
